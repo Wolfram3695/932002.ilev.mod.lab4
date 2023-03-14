c0cade4fe59eebe58f3e93dc8216c7a03b5255f1
Wolfram3695 <nikitka.ilev@mail.ru>
2023-03-14 18:18:04 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnew = new System.Windows.Forms.Button();
+            this.btstart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnew);
+            this.panel1.Controls.Add(this.btstart);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(285, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnew
+            // 
+            this.btnew.Location = new System.Drawing.Point(12, 12);
+            this.btnew.Name = "btnew";
+            this.btnew.Size = new System.Drawing.Size(99, 32);
+            this.btnew.TabIndex = 1;
+            this.btnew.Text = "Reset";
+            this.btnew.UseVisualStyleBackColor = true;
+            this.btnew.Click += new System.EventHandler(this.btnew_Click);
+            // 
+            // btstart
+            // 
+            this.btstart.Location = new System.Drawing.Point(167, 12);
+            this.btstart.Name = "btstart";
+            this.btstart.Size = new System.Drawing.Size(101, 32);
+            this.btstart.TabIndex = 0;
+            this.btstart.Text = "Start/Stop";
+            this.btstart.UseVisualStyleBackColor = true;
+            this.btstart.Click += new System.EventHandler(this.btstart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1095, 505);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Generation: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 624);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btstart;
+        private System.Windows.Forms.Button btnew;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+
+        private Graphics graph;
+        private bool[,] marks;
+        int gen = 0; 
+        int col; 
+        int row;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int clicks = 0; 
+
+        private void btstart_Click(object sender, EventArgs e)
+        {
+            if (clicks % 2 == 1)
+            {
+                btnew.Enabled = false;
+                timer1.Start(); 
+            }
+            else
+            {
+                timer1.Stop(); 
+                btnew.Enabled = true; 
+            }
+            clicks++; 
+        }
+
+        private void btnew_Click(object sender, EventArgs e)
+        {
+            graph = CreateGraphics();
+            graph.Clear(Color.White);
+            gen = 1; 
+            label1.Text = $"Generation {gen}";
+            clicks = 0;
+            col = pictureBox1.Width; 
+            row = pictureBox1.Height;
+            marks = new bool[col, row];
+            Random rand = new Random();
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    marks[i, j] = rand.Next(0, 100) > 90; 
+                }
+            }
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graph = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start(); 
+        }
+
+        private int CountNeighbours(int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var c = (x + i + col) % col;
+                    var r = (y + j + row) % row;
+                    var isSelfChecking = c == x && r == y;
+                    var hasLife = marks[c, r];
+                    if (hasLife && !isSelfChecking)
+                        count++;
+                }
+            }
+            return count;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graph.Clear(Color.White);
+            var newmarks = new bool[col, row];
+            for (int x = 0; x < col; x++)
+            {
+                for (int y = 0; y < row; y++)
+                {
+                    var neighboursCount = CountNeighbours(x, y);
+                    var hasLife = marks[x, y];
+
+                    if (!hasLife && neighboursCount == 3)
+                    {
+                        newmarks[x, y] = true;
+                    }
+                    else if (hasLife && (neighboursCount < 2 || neighboursCount > 3))
+                    {
+                        newmarks[x, y] = false;
+                    }
+                    else
+                    {
+                        newmarks[x, y] = marks[x, y];
+                    }
+                    if (hasLife)
+                    {
+                        graph.FillRectangle(Brushes.Green, x * 2, y * 2, 1, 1);
+                    }
+                }
+            }
+            marks = newmarks;
+            pictureBox1.Refresh();
+            gen++; 
+            label1.Text = $"Generation {gen}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
